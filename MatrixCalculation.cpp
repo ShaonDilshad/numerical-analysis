f41efc8d6e1f63f5f7689a95c84c6e657bd642ca
ShaonDilshad <shaon.dilshad@gmail.com>
2022-08-12 17:03:52 +0600

    MatrixCalculation.cpp

---
+++ MatrixCalculation.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 10
+int main()
+{
+    int i, j, m, n, k, p, q;
+    float a[N][N], b[N][N], c[N][N], d[N][N], e[N][N];
+
+    cout<<"enter the order of the matrices a= \n";
+    cin>>m>>n;      //scanf("%d%d",&m,&n);
+    cout<<"enter the order of the matrices b= \n";
+    cin>>p>>q;      //scanf("%d%d",&p,&q);
+
+    cout<<"enter the elements of matrix a\n";
+    for(i=1;i<=m;i++){
+        for(j=1;j<=n;j++){
+             //scanf("%f",&a[i][j]);
+             cin>>a[i][j];
+        }
+    }
+
+    cout<<"enter the elements of matrix b\n";
+    for(i=1;i<=p;i++){
+        for(j=1;j<=q;j++){
+            //scanf("%f",&b[i][j]);
+            cin>>b[i][j];
+        }
+    }
+
+
+    //Calculation Addition and Substraction
+    if(m==p && n==q){
+        for(i=1;i<=m;i++){
+            for(j=1;j<=n;j++){
+              c[i][j]=a[i][j]+b[i][j];
+              d[i][j]=a[i][j]-b[i][j];
+            }
+        }
+    }
+
+    printf("the matrices addition=\n");
+    for(i=1;i<=m;i++){
+        for(j=1;j<=n;j++){
+            //printf("%f\t",c[i][j]);
+            cout<<c[i][j]<<'\t';
+        }
+        //printf("\n\n");
+        cout<<"\n\n";
+    }
+
+    printf("the matrices subtraction=\n");
+    for(i=1;i<=m;i++){
+        for(j=1;j<=n;j++){
+            //printf("%f\t",d[i][j]);
+            cout<<d[i][j]<<'\t';
+        }
+        //printf("\n\n");
+        cout<<"\n\n";
+    }
+
+    //Matrix Multiplication
+    if(n==p){
+        for(i=1;i<=m;i++){
+            for(j=1;j<=q;j++){
+                c[i][j]=0.0;
+                for(k=1;k<=n;k++){
+                    c[i][j]=c[i][j]+a[i][k]*b[k][j];
+                }
+            }
+        }
+        printf("the matrices multiplication is=\n");
+        for(i=1;i<=m;i++)
+        {
+            for(j=1;j<=q;j++){
+                printf("%.2f\t",c[i][j]);
+                //cout<<c[i][j]<<'\t';
+            }
+            //printf("\n\n");
+            cout<<"\n\n";
+        }
+    }
+    else{
+        cout<<"the matrices multiplication is not possible"<<endl;
+    }
+
+    return 0;
+}
