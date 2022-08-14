b47722a89a5d89bbb459addf1520d4b563a7b594
ShaonDilshad <shaon.dilshad@gmail.com>
2022-08-14 21:30:07 +0600

    MatrixCalculation modified

--- MatrixCalculation.cpp
+++ MatrixCalculation.cpp
@@ -1,7 +1,10 @@
+//Matrix Calculation : addition, substraction & multiplication
+
 #include <bits/stdc++.h>
 using namespace std;
 
 #define N 10
+
 int main()
 {
     int i, j, m, n, k, p, q;
@@ -12,48 +15,52 @@ int main()
     cout<<"enter the order of the matrices b= \n";
     cin>>p>>q;      //scanf("%d%d",&p,&q);
 
+    if ((m != p) && (n != q))
+    {
+        cout << "Matrices cannot be added or subtracted!";
+        exit(0);
+    }	
+
     cout<<"enter the elements of matrix a\n";
-    for(i=1;i<=m;i++){
-        for(j=1;j<=n;j++){
-             //scanf("%f",&a[i][j]);
-             cin>>a[i][j];
+    for(i=1; i<=m; i++){
+        for(j=1; j<=n; j++){
+            //scanf("%f", &a[i][j]);
+            cin>>a[i][j];
         }
     }
 
     cout<<"enter the elements of matrix b\n";
-    for(i=1;i<=p;i++){
-        for(j=1;j<=q;j++){
-            //scanf("%f",&b[i][j]);
+    for(i=1; i<=p; i++){
+        for(j=1; j<=q; j++){
+            //scanf("%f", &b[i][j]);
             cin>>b[i][j];
         }
     }
 
 
     //Calculation Addition and Substraction
-    if(m==p && n==q){
-        for(i=1;i<=m;i++){
-            for(j=1;j<=n;j++){
-              c[i][j]=a[i][j]+b[i][j];
-              d[i][j]=a[i][j]-b[i][j];
-            }
+    for(i=1; i<=m; i++){
+        for(j=1; j<=n; j++){
+            c[i][j] = a[i][j] + b[i][j];
+            d[i][j] = a[i][j] - b[i][j];
         }
     }
 
     printf("the matrices addition=\n");
-    for(i=1;i<=m;i++){
-        for(j=1;j<=n;j++){
-            //printf("%f\t",c[i][j]);
-            cout<<c[i][j]<<'\t';
+    for(i=1; i<=m; i++){
+        for(j=1; j<=n; j++){
+            //printf("%f\t", c[i][j]);
+            cout<<'\t'<<c[i][j]<<'\t';
         }
         //printf("\n\n");
         cout<<"\n\n";
     }
 
     printf("the matrices subtraction=\n");
-    for(i=1;i<=m;i++){
-        for(j=1;j<=n;j++){
-            //printf("%f\t",d[i][j]);
-            cout<<d[i][j]<<'\t';
+    for(i=1; i<=m; i++){
+        for(j=1; j<=n; j++){
+            //printf("%f\t", d[i][j]);
+            cout<<'\t'<<d[i][j]<<'\t';
         }
         //printf("\n\n");
         cout<<"\n\n";
@@ -61,19 +68,18 @@ int main()
 
     //Matrix Multiplication
     if(n==p){
-        for(i=1;i<=m;i++){
-            for(j=1;j<=q;j++){
+        for(i=1; i<=m; i++){
+            for(j=1; j<=q; j++){
                 c[i][j]=0.0;
-                for(k=1;k<=n;k++){
-                    c[i][j]=c[i][j]+a[i][k]*b[k][j];
+                for(k=1; k<=n; k++){
+                    c[i][j] = c[i][j] + a[i][k] * b[k][j];
                 }
             }
         }
-        printf("the matrices multiplication is=\n");
-        for(i=1;i<=m;i++)
-        {
-            for(j=1;j<=q;j++){
-                printf("%.2f\t",c[i][j]);
+        printf("the matrices multiplication are=\n");
+        for(i=1; i<=m; i++){
+            for(j=1; j<=q; j++){
+                printf("%.2f\t", c[i][j]);
                 //cout<<c[i][j]<<'\t';
             }
             //printf("\n\n");
@@ -81,7 +87,7 @@ int main()
         }
     }
     else{
-        cout<<"the matrices multiplication is not possible"<<endl;
+        cout<<"the matrices multiplication are not possible"<<endl;
     }
 
     return 0;
